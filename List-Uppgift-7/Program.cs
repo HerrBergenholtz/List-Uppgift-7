93c9bdc8c74465a69ec3c1042f748c7aa23f7ed8
HerrBergenholtz <zbdv8800@gmail.com>
2024-02-06 10:25:46 +0100

    Add project files.

---
+++ List-Uppgift-7/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Uppgift_7
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] snus = { "a", "b", "b", "c", "hej" };
+            string[] snus2 = { "hej", "a", "a", "b", "programmering" };
+
+            foreach (string snusmus in FinnsIBåda(snus, snus2))
+            {
+                Console.Write($"\"{snusmus}\" ");
+            }
+        }
+
+        static string[] FinnsIBåda(string[] arr1, string[] arr2)
+        {
+            List<string> lista = new();
+
+            foreach (string str in arr1)
+            {
+                if (arr2.Contains(str) && !lista.Contains(str))
+                {
+                    lista.Add(str);
+                }
+            }
+
+            return lista.ToArray();
+        }
+    }
+}
